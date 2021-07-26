b8eeb8c675bdda398078bd6e06185a75c7b6b96f
Piero <piero.onorati@virgilio.it>
2021-07-26 15:11:51 +0200

    complete Snack1

--- snack1.php
+++ snack1.php
@@ -1,4 +1,35 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
 <?php 
+
+$campionato = [
+    [
+        'squadraCasa' => 'Francia',
+        'squadraOspite' => 'Stati Uniti',
+        'puntiCasa' => 83,
+        'puntiOspite' => 76,
+    ],
+    [
+        'squadraCasa' => 'Slovenia',
+        'squadraOspite' => 'Argentina',
+        'puntiCasa' => 118,
+        'puntiOspite' => 100,
+    ],
+    [
+        'squadraCasa' => 'Italia',
+        'squadraOspite' => 'Germania',
+        'puntiCasa' => 92,
+        'puntiOspite' => 82,
+    ],
+    [
+        'squadraCasa' => 'Australia',
+        'squadraOspite' => 'Nigeria',
+        'puntiCasa' => 84,
+        'puntiOspite' => 67,
+    ]
+]
 
 ?>
 
@@ -11,6 +42,21 @@
     <title>Document</title>
 </head>
 <body>
+
+<ul>
+    
+    <?php for ($i=0; $i < count($campionato) ; $i++) { ?>
+
+        <li>
+            <?php echo $campionato[$i]['squadraCasa'] ?> - 
+            <?php echo $campionato[$i]['squadraOspite'];?> | 
+            <?php echo $campionato[$i]['puntiCasa'];?>-
+            <?php echo $campionato[$i]['puntiOspite'];?>
+        </li>
+
+    <?php } ?>
+
+</ul>
     
 </body>
 </html>
