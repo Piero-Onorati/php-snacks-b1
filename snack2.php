a6a3c33065af58bdb5a00087f3d11dcf5e5aa4ab
Piero <piero.onorati@virgilio.it>
2021-07-26 16:04:32 +0200

    complete snack2

--- snack2.php
+++ snack2.php
@@ -1,6 +1,34 @@
+<!-- Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
 <?php
 
+$name = $_GET['name'];
+$age = $_GET['age'];
+$email = $_GET['email'];
+// var_dump($name);
+// var_dump($age);
+// var_dump($email);
+
+if (empty($_GET['name']) || empty($_GET['age']) || empty($_GET['email'])) {
+    $stringa = 'all the fields must be completed';
+    
+} else {
+    if( strlen($name)>3 && 
+    strpos($email, '@') && 
+    strpos($email, '.') && 
+    is_numeric($age)
+    ){
+        $stringa = 'accesso consentito';
+    } else {
+        $stringa = 'accesso negato';
+    }
+
+}
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +39,15 @@
     <title>Document</title>
 </head>
 <body>
+
+<form action="" method="get" id="form1">
+    <input type="text" name="name">
+    <input type="text" name="age"> 
+    <input type="text" name="email"> 
+    <button type="submit" form="form1">SUBMIT</button>
+</form>
+
+<p> esito: <?php echo $stringa; ?></p>
     
 </body>
 </html>
